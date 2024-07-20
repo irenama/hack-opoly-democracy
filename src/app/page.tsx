5abd2a7199b245f91adba1768ae610c869ea3b9d
Irena Reitz <irena.reitz@peerigon.com>
2024-07-21 00:38:17 +0200

    brr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import CharacterCard from "@/components/CharacterCard";
 import Section from "@/components/Section";
 
--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -3,10 +3,10 @@ import { type Character } from "@/app/page";
 const CharacterCard: React.FC<{ data: Omit<Character, "id"> }> = ( { data } ) => {
   return (
     <article className='flex flex-col shadow-lg bg-white border-black border-4 w-80'>
-      <div className="bg-blue-800 h-20 border-b-2 border-black"></div>
-      <div className="p-5">
+      <div className="bg-blue-800 h-32 border-b-2 border-black"></div>
+      <div className="p-5 bg-green-200 h-full">
 
-        <div className="min-h-40 py-5 text-center flex justify-center items-center">
+        <div className="min-h-32 py-5 text-center flex justify-center items-center">
           <h4>{data.name}</h4>
         </div>
         <ul className="pt-2 h-auto">
