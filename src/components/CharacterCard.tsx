855caf706f7807a901ad7b203f0e4377055474bf
Irena Reitz <irena.reitz@peerigon.com>
2024-07-20 23:48:35 +0200

    update character cards

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -4,10 +4,10 @@ const CharacterCard: React.FC<{ data: Omit<Character, "id"> }> = ( { data } ) =>
   return (
     <article className='flex flex-col shadow-lg bg-white border-black border-4 w-80'>
       <div className="bg-red-500 h-20 border-b-2 border-black"></div>
-      <div className="p-2">
+      <div className="p-5">
 
-        <div className="h-40 flex justify-center align-middle">
-          <h4>{data.name}</h4>
+        <div className="min-h-40 py-5 text-center flex justify-center items-center">
+          <h3>{data.name}</h3>
         </div>
         <ul className="pt-2 h-auto">
           {data.characteristics.map( ( characteristic, index ) => (
