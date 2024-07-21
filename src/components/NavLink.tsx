ec68598eb03260f2876064249d3d22d06b6bb2cc
Irena Reitz <irena.reitz@peerigon.com>
2024-07-21 13:59:44 +0200

    highlight nav

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const NavLink: React.FC<{ href: string; text: string }> = ( { href, text } ) => {
   return (
-    <nav><Link href={href} className="absolute right-5 top-5">{text}</Link></nav>
+    <nav className="fixed top-0 left-0 right-0 bg-green-200 shadow-sm p-5 flex justify-end border-2 border-black"><Link href={href} >{text}</Link></nav>
   );
 }
 
